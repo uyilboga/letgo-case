2b8f3a77f628d9e1d637cfbb7591ac55be640657
ufuk <ufukyilboga@lastikcim.com.tr>
2023-12-20 15:45:00 +0300

    first commit

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,7 +2,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { rootReducer } from '@/store/rootReducer';
-import { thunk } from 'redux-thunk';
 
 const persistConfig = {
   key: 'root',
